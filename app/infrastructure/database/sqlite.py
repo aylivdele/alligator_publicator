ffef795340d9706d9301fa54d549a61d7cdd2a2e
aylivdele <irassomahin@gmail.com>
2026-03-01 15:05:46 +0500

    fixes

--- app/infrastructure/database/sqlite.py
+++ app/infrastructure/database/sqlite.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./instagram_tokens.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////data/sqlite.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
