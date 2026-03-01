30511a4f17904b10d2eeb7750e78b0021d5cffb2
aylivdele <irassomahin@gmail.com>
2026-03-01 15:28:16 +0500

    init db

--- app/infrastructure/database/sqlite.py
+++ app/infrastructure/database/sqlite.py
@@ -1,12 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from app.domain.models import Base
+
 SQLALCHEMY_DATABASE_URL = "sqlite:////data/sqlite.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
+
+Base.metadata.create_all(engine)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
